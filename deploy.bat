89855c4de19b8370acc2d15d206c0296f2ad49d2
Will Rogers <wjrogers@gmail.com>
2018-07-07 22:16:51 -0400

    Deployment script for updating the server

---
+++ deploy.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+ECHO %~1
+
+IF "%~1" == "" (
+    ECHO Missing destination directory!
+    EXIT /b 2
+)
+
+ECHO Deploying modpack to "%~1"; press CTRL+C to abort...
+PAUSE
+
+ROBOCOPY /Z /E /MIR config "%~1\config"
+ECHO.
+
+ROBOCOPY /Z /E /MIR mods "%~1\mods"
+ECHO.
+
+ROBOCOPY /Z /E /MIR scripts "%~1\scripts"
+ECHO.
